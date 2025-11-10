92bd28058073bc7fe2a39d1476c0767d6f4d9278
luisda23 <sanchisdaniel1@gmail.com>
2025-11-10 17:13:17 +0100

    Versió 1.2 del nou projecte pizzeria - amb docker-compose.yml

---
+++ codis/pizzeria/app/pizza.php
@@ -0,0 +1,18 @@
+<?php
+	$pvp = 10;
+	$IVA = 21;
+	if (empty($_GET['ingr'])) {
+		echo "Has demanat només la pizza bàsica. El preu és $pvp_public=$pvp*((100+$IVA)/100)";
+		echo "El preu de la pizza bàsica és $pvp_public € (IVA inclòs)<br>";
+	}
+	else {
+		$pvp_public=((100+$IVA)/100)*($pvp+count($_GET['ingr']));
+		echo "Has demanat la pizza bàsica amb:<br> ";
+		foreach ($_GET['ingr'] as $ingredient) {
+			if ($ingredient) {
+				echo "* $ingredient<br>";
+			}
+		}
+		echo "El preu és $pvp_public € (IVA inclòs)<br>";
+	}
+?>
